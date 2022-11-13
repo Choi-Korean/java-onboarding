ae0a43bfe1c932f41b635c147e34c7ca1e244f2c
cho2 <chso1324@naver.com>
2022-11-14 01:51:28 +0900

    P5 화폐개수 n개로 채워진 answer 리스트에 화폐개수 차례대로 넣기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,39 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(Collections.nCopies(9, 0));
+        int count;
+        int n = 1;
+        int curr = 50000;
+
+        for(int i = 0; i < 9; i ++){
+            count = money / curr;
+            answer.set(i, count);
+            money -= count * curr;
+
+            if(money == 0){
+                break;
+            }
+
+            n += 1;
+            if(n == 9){
+                curr = 1;
+                continue;
+            }
+            if(n % 2 == 0){
+                curr /= 5;
+                continue;
+            }
+
+            curr /= 2;
+            continue;
+        }
+
         return answer;
     }
 }
